91a70ec72f059919023cb1780f82cf46a616847b
dqmis <dom.seputis@gmail.com>
2024-04-20 12:21:52 +0200

    Added inference pipeline

---
+++ src/metrics/_base_metric.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from numpy.typing import NDArray
+
+
+class Metric(ABC):
+
+    @abstractmethod
+    def evaluate(self, y_true: NDArray[Any], y_pred: NDArray[Any]) -> float:
+        pass
