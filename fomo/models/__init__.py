040771d995670ccfa09a50952330b4b7c998fe55
dqmis <dom.seputis@gmail.com>
2024-04-29 22:13:30 +0300

    updated model base

--- fomo/models/__init__.py
+++ fomo/models/__init__.py
@@ -1,12 +1,14 @@
 from enum import Enum
+from functools import partial
 
 from fomo.models._base_model import BaseModel
 from fomo.models.clip_classifier import ClipClassifier
 
 
 class Model(Enum):
-    CLIP_VIT_BASE_PATCH32_PRETRAINED = ClipClassifier(model_base="openai/clip-vit-base-patch32")
+    CLIP_VIT_BASE_PATCH16_PRETRAINED = partial(lambda: ClipClassifier("ViT-B/16"))
 
     @classmethod
     def from_str(cls, name: str) -> BaseModel:
-        return cls[name.upper()].value
+        model: BaseModel = cls[name.upper()].value()
+        return model
--- fomo/models/clip_classifier.py
+++ fomo/models/clip_classifier.py
@@ -1,46 +1,50 @@
 from typing import Any
 
 import numpy as np
+import torch
 from numpy.typing import NDArray
 from torch.utils.data import DataLoader
+from torchvision.transforms import Compose
 from tqdm import tqdm
-from transformers import CLIPModel, CLIPProcessor
 
 from fomo.models._base_model import BaseModel
+from fomo.models.clip.clip_base import ClipBase
 
 
 class ClipClassifier(BaseModel):
     def __init__(self, model_base: str, class_template: str | None = None) -> None:
-        self._model = CLIPModel.from_pretrained(model_base)
-        self._processor = CLIPProcessor.from_pretrained(model_base)
+        self._model = ClipBase(model_base)
+        self._model.to_cpu()
+        self._model.eval()
+
         self._class_propmts: list[str] | None = None
 
         self._class_template = class_template or "a photo of a {}"
 
     @property
     def batch_size(self) -> int:
-        return 32
+        return 2
+
+    @property
+    def transforms(self) -> Compose:
+        return self._model.transforms
 
     def reconfig_labels(self, labels: list[str]) -> None:
-        self._class_propmts = self._build_class_prompt(labels)
+        prompts = self._build_class_prompt(labels)
+        self._model.precompute_prompt_features(prompts)
 
     def _build_class_prompt(self, class_names: list[str]) -> list[str]:
         class_template = self._class_template
         return [class_template.format(class_name) for class_name in class_names]
 
     def predict_for_eval(self, x: DataLoader[Any]) -> tuple[NDArray[Any], NDArray[Any]]:
-        assert self._class_propmts
-
         predictions = []
         targets = []
         for batch in tqdm(x):
             images, batch_targets = batch
 
-            inputs = self._processor(
-                text=self._class_propmts, images=images, return_tensors="pt", padding=True, truncation=True
-            )
-            outputs = self._model(**inputs)
-            logits_per_image = outputs.logits_per_image
+            with torch.no_grad():
+                logits_per_image = self._model.forward(images)
             probs = logits_per_image.softmax(dim=1)
             predictions.extend(probs.argmax(dim=1).cpu().numpy())
             targets.extend(batch_targets)
