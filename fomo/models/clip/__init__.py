9386413fdd101831df4c54805235dcff28a9fc58
SergheiMihailov <mihailov.serghei.d@gmail.com>
2024-05-08 19:08:40 +0200

    Add basic mlp adapters

--- fomo/models/clip/__init__.py
+++ fomo/models/clip/__init__.py
@@ -1,7 +1,11 @@
 from fomo.models.clip.clip_base import ClipBase
 from fomo.models.clip.clip_linear import ClipLinear
+from fomo.models.clip.clip_mlp_head import CLIPMLPHead
+from fomo.models.clip.clip_mm_mlp_adapter import CLIPMMMLPAdapter
 
 MODELS = {
     "clip_base": ClipBase,
     "clip_linear": ClipLinear,
+    "clip_mm_mlp": CLIPMLPHead,
+    "clip_mm_mlp_adapter": CLIPMMMLPAdapter,
 }
