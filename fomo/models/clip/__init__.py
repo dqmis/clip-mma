d6a1684400f297da9651fad84403b5468bb0c799
Serghei <serghei@zino-hofmann.com>
2024-05-18 18:59:54 +0200

    feat: add multiple mm adapter models; add hp-search notebook

--- fomo/models/clip/__init__.py
+++ fomo/models/clip/__init__.py
@@ -1,13 +1,19 @@
+from fomo.models.clip.clip_adapter import CLIPAdapter
 from fomo.models.clip.clip_base import ClipBase
 from fomo.models.clip.clip_linear import ClipLinear
-from fomo.models.clip.clip_transformer import ClipTransformer
 from fomo.models.clip.clip_mlp_head import CLIPMLPHead
 from fomo.models.clip.clip_mm_mlp_adapter import CLIPMMMLPAdapter
+from fomo.models.clip.clip_transformer import ClipTransformer
+from fomo.models.clip.clip_transformer_downscaled import ClipTransformerDownscaled
+from fomo.models.clip.clip_transformer_w_hypernet import ClipTransformerWHypernet
 
 MODELS = {
     "clip_base": ClipBase,
     "clip_linear": ClipLinear,
     "clip_transformer": ClipTransformer,
+    "clip_transformer_downscaled": ClipTransformerDownscaled,
     "clip_mm_mlp": CLIPMLPHead,
     "clip_mm_mlp_adapter": CLIPMMMLPAdapter,
+    "clip_transformer_w_hypernet": ClipTransformerWHypernet,
+    "clip_adapter": CLIPAdapter,
 }
