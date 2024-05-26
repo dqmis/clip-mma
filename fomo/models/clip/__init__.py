98265b9c5d77362d39e3bfd6138ece555067700e
dqmis <dom.seputis@gmail.com>
2024-05-26 09:49:09 +0000

    Updated code

--- fomo/models/clip/__init__.py
+++ fomo/models/clip/__init__.py
@@ -5,6 +5,7 @@ from fomo.models.clip.clip_mlp_head import CLIPMLPHead
 from fomo.models.clip.clip_mm_mlp_adapter import CLIPMMMLPAdapter
 from fomo.models.clip.clip_transformer import ClipTransformer
 from fomo.models.clip.clip_transformer_adapter import CLIPTransformerAdapter
+from fomo.models.clip.clip_transformer_adapter_text import CLIPTransformerAdapterText
 from fomo.models.clip.clip_transformer_downscaled import ClipTransformerDownscaled
 from fomo.models.clip.clip_transformer_w_hypernet import ClipTransformerWHypernet
 
@@ -17,5 +18,6 @@ MODELS = {
     "clip_mm_mlp_adapter": CLIPMMMLPAdapter,
     "clip_transformer_w_hypernet": ClipTransformerWHypernet,
     "clip_transformer_adapter": CLIPTransformerAdapter,
+    "clip_transformer_adapter_text": CLIPTransformerAdapterText,
     "clip_adapter": CLIPAdapter,
 }
