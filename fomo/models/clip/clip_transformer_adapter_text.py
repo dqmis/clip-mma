d7a851e6a1d65e9f78038fd72ffbf571a7d8362b
dqmis <dom.seputis@gmail.com>
2024-05-26 09:49:09 +0000

    Updated code

--- fomo/models/clip/clip_transformer_adapter_text.py
+++ fomo/models/clip/clip_transformer_adapter_text.py
@@ -10,22 +10,36 @@ from fomo.models.clip.modules.masked_multi_head_attention_downsampled import (
 )
 
 
+class Adapter(nn.Module):
+    def __init__(self, c_in, reduction=4):
+        super(Adapter, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(c_in, c_in // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(c_in // reduction, c_in, bias=False),
+            nn.ReLU(inplace=True)
+        )
 
-class CLIPTransformerAdapter(ClipBase):
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+
+class CLIPTransformerAdapterText(ClipBase):
     """Reproduction of CLIP-Adapter"""
 
     def __init__(self, backbone: str = "ViT-B/16", root: str = "./data") -> None:
         # pass default arguments to the parent class
-        super(CLIPTransformerAdapter, self).__init__(backbone, root=root)
+        super(CLIPTransformerAdapterText, self).__init__(backbone, root=root)
 
         self.image_encoder = self._clip.visual
         self.logit_scale = self._clip.logit_scale
         self.adapter = MaskedMultiheadAttentionDownsampled()
+        self.image_adapter = Adapter(512, 4).to(torch.float32)
 
     @property
     def learnable_param_names(self) -> set[str]:
         # IMPORTANT: Add the name of the learnable parameters in the model
-        return set(["adapter"])
+        return set(["adapter", "image_adapter"])
 
     # If needed you can override the to_cpu and to_cuda methods
     def to_cpu(self) -> None:
@@ -35,6 +49,7 @@ class CLIPTransformerAdapter(ClipBase):
 
     def to_cuda(self) -> None:
         self.adapter.to(torch.device("cuda"))
+        self.image_adapter.to(torch.device("cuda"))
         self._clip.to(torch.device("cuda"))
 
     def forward(self, images: torch.Tensor, prompts: list[str] | None = None) -> torch.Tensor:
@@ -50,28 +65,39 @@ class CLIPTransformerAdapter(ClipBase):
         text_features = text_features.to(torch.float32)  # [n_classes, rep_dim]
 
         num_classes = text_features.shape[0]
+        
+        text_features = text_features.unsqueeze(1).expand(-1, image_features.shape[0], -1)
+        image_features = image_features.unsqueeze(0)
 
         adapter_input = torch.cat(
-            [text_features.unsqueeze(1).expand(-1, image_features.shape[0], -1), image_features.unsqueeze(0)],
+            [text_features, image_features],
             dim=0,
         )
 
         adapter_output = self.adapter(adapter_input)
 
-        adapter_image_features = adapter_output[num_classes:].squeeze(0)
+        adapter_image_features = adapter_output[num_classes:]
         adapter_text_features = adapter_output[:num_classes]
+        
+        adapter_image_features = self.image_adapter(adapter_image_features)
         
         adapter_image_features = adapter_image_features / adapter_image_features.norm(dim=-1, keepdim=True)
         adapter_text_features = adapter_text_features / adapter_text_features.norm(dim=-1, keepdim=True)
         
+        
         image_ratio = 0.2
+        text_ratio = 0.2
+        
+        image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+        text_features = text_features / text_features.norm(dim=-1, keepdim=True)
         
         image_features = image_ratio * adapter_image_features + (1 - image_ratio) * image_features
-
+        text_features = text_ratio * adapter_text_features  + (1 - text_ratio) * text_features
+        
         image_features = image_features / image_features.norm(dim=-1, keepdim=True)
         text_features = text_features / text_features.norm(dim=-1, keepdim=True)
 
         logit_scale = self.logit_scale.exp()
-        logits_per_image: torch.Tensor = logit_scale * image_features @ text_features.t()
+        logits_per_image: torch.Tensor = logit_scale * torch.bmm(image_features.permute(1, 0, 2), text_features.permute(1, 2, 0)).squeeze(1)
 
         return logits_per_image
