4414371eae37323564ecb167deb7f587944dddc3
Serghei <serghei@zino-hofmann.com>
2024-06-15 20:24:51 +0200

    feat: add models for ablating bottleneck and transformer vs mha

---
+++ fomo/models/clip/ablations/clip_mha_adapter.py
@@ -0,0 +1,13 @@
+from fomo.models.clip.clip_transformer_adapter_text import CLIPTransformerAdapterText
+from fomo.models.clip.modules.masked_multi_head_attention_downsampled import (
+    MaskedMultiheadAttentionDownsampled,
+)
+
+
+class CLIPMhaAdapter(CLIPTransformerAdapterText):
+    """Reproduction of CLIP-Adapter"""
+
+    def __init__(self, backbone: str = "ViT-B/16", root: str = "./data") -> None:
+        # pass default arguments to the parent class
+        super(CLIPMhaAdapter, self).__init__(backbone, root=root)
+        self.adapter = MaskedMultiheadAttentionDownsampled(block_type="mha")
--- fomo/pipelines/types/learner_args.py
+++ fomo/pipelines/types/learner_args.py
@@ -7,23 +7,23 @@ from typing import Any
 
 @dataclass
 class LearnerArgs:
-    use_wandb: bool = False
+    use_wandb: bool = True
     epochs: int = 100
-    patience: int = 10
+    patience: int = 5
     model_type: str = "clip_base"
-    print_freq: int = 10
-    save_freq: int = 10
+    print_freq: int = 200
+    save_freq: int = 200
     output_dir: str = "./output/"
     model_backbone: str = "ViT-B/16"
     dataset: str = "cifar10"
     device: str = "cuda"
-    batch_size: int = 64
+    batch_size: int = 256
     num_workers: int = 4
     train_size: float | None = None
     train_eval_size: tuple[int, int] | None = None
     text_prompt_template: str = "a photo of {}."
     learning_rate: float = 0.01
-    momentum: float = 0.9
+    momentum: float = 0.5
     weight_decay: float = 1e-4
     warmup: int = 0
     info: str | None = None
